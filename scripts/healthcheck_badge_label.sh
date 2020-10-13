7221f0cb0a0e0205426e5ed471d2b59d48443552
Rupert Barrow <AltiusRupert@users.noreply.github.com>
2020-10-13 22:09:42 +0200

    Create healthcheck_badge_label.sh

---
+++ scripts/healthcheck_badge_label.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#./reports/healthcheck.json
+
+FILE=$1 
+
+HIGHRISKITEMS=$(          cat $FILE | jq -r ' .highriskitems[]'          | wc -l | xargs)
+MEDIUMRISKITEMS=$(        cat $FILE | jq -r ' .mediumriskitems[]'        | wc -l | xargs)
+LOWISKITEMS=$(            cat $FILE | jq -r ' .lowriskitems[]'           | wc -l | xargs)
+INFORMATIONALRISKITEMS=$( cat $FILE | jq -r ' .informationalriskitems[]' | wc -l | xargs)
+DT=$( date )
+
+echo "H:${HIGHRISKITEMS} M:${MEDIUMRISKITEMS} l:${LOWISKITEMS} i:${INFORMATIONALRISKITEMS} ($DT)"

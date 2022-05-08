adeda8b99ca7a7ad9734b4b89d9a3d98c019c364
Rupert Barrow <48629751+RupertBarrow@users.noreply.github.com>
2022-05-08 11:39:53 +0200

    Format de date

--- action-create-salesforce-badges/scripts/healthcheck_badge_label.sh
+++ action-create-salesforce-badges/scripts/healthcheck_badge_label.sh
@@ -7,7 +7,7 @@ HIGHRISKITEMS=$(          cat $FILE | jq -r ' .highriskitems[]'          | wc -l
 MEDIUMRISKITEMS=$(        cat $FILE | jq -r ' .mediumriskitems[]'        | wc -l | xargs > /tmp/res.txt && cat /tmp/res.txt)
 LOWRISKITEMS=$(           cat $FILE | jq -r ' .lowriskitems[]'           | wc -l | xargs > /tmp/res.txt && cat /tmp/res.txt)
 INFORMATIONALRISKITEMS=$( cat $FILE | jq -r ' .informationalriskitems[]' | wc -l | xargs > /tmp/res.txt && cat /tmp/res.txt)
-DT=$( date )
+DT=$( date "+%a %d %b %Y, %H:%M %Z" )
 
 if [ $HIGHRISKITEMS == 'null' ] || [ $MEDIUMRISKITEMS == 'null' ] || [ $LOWRISKITEMS == 'null' ] || [ $INFORMATIONALRISKITEMS == 'null' ]
 then

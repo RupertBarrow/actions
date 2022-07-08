dfad24579cc09a20de041b25dce8149d25443ead
RupertBarrow <rupert.barrow@lilo.org>
2022-07-09 00:12:40 +0200

    Quotes and escaing

--- action-git2devopscenter/scripts/create-work-item.sh
+++ action-git2devopscenter/scripts/create-work-item.sh
@@ -25,8 +25,8 @@ projectid=$(sfdx force:data:soql:query -q "SELECT Id FROM sf_devops__Project__c 
 # Création du work item
 workitemid=$(sfdx force:data:record:create -s sf_devops__Work_Item__c -v "sf_devops__Subject__c='$subject' sf_devops__Project__c=$projectid sf_devops__Description__c='$body'" --json | jq '.result.id')
 # Exemple : workitemid='"a3N1i000000acNmEAI"'
-# On retire les ''
-workitemid=$(echo $workitemid)
+# On retire les ""
+workitemid=$(echo $workitemid | sed -r "s/\"+//g")
 
 # Recherche du nom du work item
 workitemname=$(sfdx force:data:record:get -s sf_devops__Work_Item__c -i $workitemid                                                                                              --json | jq '.result.Name')
